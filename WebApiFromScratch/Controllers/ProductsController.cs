bdc63be90357de33e84bb8297d2291596930f3d5
synset <synnove.settman@yahoo.com>
2020-09-01 15:17:21 +0200

    WebApiFromScratch

---
+++ WebApiFromScratch/Controllers/ProductsController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApiFromScratch.Models;
+
+namespace WebApiFromScratch.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        List<Product> products = new List<Product>()
+        {
+            new Product {
+                Id = 1006368,
+                Name = "Austin and Barbeque",
+                Description = "Thermometer",
+                Price = 399
+            },
+
+            new Product {
+                Id = 1009334,
+                Name = "Andersson",
+                Description = "Tändare",
+                Price = 89
+            },
+
+            new Product {
+                Id = 1002266,
+                Name = "Weber",
+                Description = "Oil",
+                Price = 99
+            }
+        };
+
+
+        [HttpGet] //Get ALL products
+        public IEnumerable<Product> Get()
+        {
+            return products;
+        }
+    }
+}
---
+++ WebApiFromScratch/Models/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiFromScratch.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+
+        public Product()
+        {
+
+        }
+
+        public Product(int id, string name, string description, double price)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            Price = price;
+        }
+    }
+}
